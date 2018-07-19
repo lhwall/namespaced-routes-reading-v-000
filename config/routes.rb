1daf82f8761802a9a9f3f9cd93d3b38294ac2afc
Lucy Wall <lhwall@gmail.com>
2018-07-19 19:44:47 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
-scope "/admin" do
-  resources: stats, only: [:index]
-root 'posts#index'
+  root 'posts#index'
+
+  scope "/admin" do
+    resources: stats, only: [:index]
+
 end
